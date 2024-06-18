2ef88ff77e674cde2b28ec127f2f731318090f29
AdrianoJr07 <adrianoferreira41@outlook.com>
2024-06-18 12:27:59 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Volta-ao-Mundo
+## Argentina: Beleza, Culinária e Paixão pelo Futebol
+
+Bem-vindo ao repositório dedicado à Argentina, um país vibrante e apaixonante que encanta por sua diversidade cultural, culinária rica e tradição no futebol. Neste espaço, exploraremos os aspectos mais fascinantes deste país sul-americano que conquista corações ao redor do mundo.
+
+### Beleza Natural e Cultural
+
+A Argentina é um país de paisagens deslumbrantes, desde os picos nevados dos Andes até as vastas planícies da Pampa. Seus contrastes naturais são um convite à aventura e à contemplação. Além disso, suas cidades como Buenos Aires e Córdoba exibem uma arquitetura magnífica e uma vida cultural vibrante, onde o tango ressoa nas ruas e os museus guardam séculos de história.
+
+### Culinária Tradicional e Gourmet
+
+A gastronomia argentina é uma celebração de sabores intensos e técnicas culinárias únicas. Das carnes suculentas dos famosos churrascos argentinos às empanadas recheadas de sabor, cada prato conta uma história de tradição e paixão pela boa comida. Não podemos esquecer do mate, a bebida nacional que une pessoas em rodas de conversa por todo o país.
+
+### Pontos Turísticos Imperdíveis
+
+Se você está planejando uma viagem à Argentina, não pode deixar de visitar lugares como as Cataratas do Iguaçu, a geleira Perito Moreno, a colorida La Boca em Buenos Aires, e as vinícolas de Mendoza, onde você pode degustar alguns dos melhores vinhos da América do Sul.
+
+### Vinícolas e Vinhos Argentinos
+
+Mendoza é o coração da produção de vinho argentino, famosa por seus Malbecs robustos e tintos encorpados que ganham prêmios internacionais. As vinícolas da região oferecem tours que não apenas mostram o processo de vinificação, mas também permitem que você saboreie o resultado final em cenários deslumbrantes de vinhedos e montanhas.
+
+### Seleção Argentina de Futebol e Lionel Messi
+
+Para os argentinos, o futebol é mais do que um esporte; é uma paixão nacional. A seleção argentina, com seus momentos históricos e seus ícones como Lionel Messi, é motivo de orgulho para todos os argentinos. Messi, um dos maiores jogadores de todos os tempos, personifica a habilidade, a determinação e o talento que caracterizam o futebol argentino.
+
+Este repositório é um tributo à Argentina, um convite para explorar sua beleza, provar sua culinária, visitar seus pontos turísticos deslumbrantes, descobrir seus vinhos excepcionais e celebrar sua paixão pelo futebol. Sinta-se à vontade para contribuir, compartilhar suas experiências e se inspirar na riqueza deste país tão extraordinário.
--- teste.sql
+++ teste.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15-Jun-2024 às 02:33
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.1.12
+-- Tempo de geração: 18/06/2024 às 05:59
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,43 +24,163 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura para tabela `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `comentarios` varchar(200) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `comentario`
+--
+
+INSERT INTO `comentario` (`comentarios`, `Id`, `id_user`, `status`) VALUES
+('teste', 21, 20, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `moisesinho`
+--
+
+CREATE TABLE `moisesinho` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `moisesinho`
+--
+
+INSERT INTO `moisesinho` (`id`, `tipo`) VALUES
+(1, 'admin'),
+(2, 'usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `status`
+--
+
+CREATE TABLE `status` (
+  `Id` int(11) NOT NULL,
+  `status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `status`
+--
+
+INSERT INTO `status` (`Id`, `status`) VALUES
+(1, 'aprovado'),
+(2, 'reprovado');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `senha` varchar(20) NOT NULL
+  `email` varchar(50) NOT NULL,
+  `senha` text NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `datacad` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Despejando dados para a tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'adriano', 'adriano@1.com', '12345'),
-(2, 'AAQAQ', 'batata@12.com', '1234');
+INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `tipo`, `datacad`) VALUES
+(9, 'Jorge', 'adrianojunior00512@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 2, '16-06-2024'),
+(13, 'Jorge neto', 'd.a.s+@online24.maskmy.id', '35a9e381b1a27567549b5f8a6f783c167ebf809f1c4d6a9e367240484d8ce281', 2, '18-06-2024'),
+(14, 'Teupai', 'adriano@1.com', '8bb0cf6eb9b17d0f7d22b456f121257dc1254e1f01665370476383ea776df414', 1, '18-06-2024'),
+(18, 'marco', 'marcola@gmail.com', '$2y$10$VB1OkVjPwqWQ5UVDzjeXuObI2VRblOywwZk.IZrqcifCBTah5WwOS', 1, ''),
+(19, 'teteu', 'teteu24@gmail', '$2y$10$1uJVxYNc5Uef7YYnyNk8eOHDtt5O8YIKase.svTlylsRYYKwUHrRG', 2, ''),
+(20, 'sid', 'sid07@gmail.com', 'eaf89db7108470dc3f6b23ea90618264b3e8f8b6145371667c4055e9c5ce9f52', 2, '18-06-2024');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `usuario`
+-- Índices de tabela `comentario`
 --
-ALTER TABLE `usuario`
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `fk_usuario` (`id_user`),
+  ADD KEY `fk_status` (`status`);
+
+--
+-- Índices de tabela `moisesinho`
+--
+ALTER TABLE `moisesinho`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- Índices de tabela `status`
 --
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_moisesinho` (`tipo`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de tabela `moisesinho`
+--
+ALTER TABLE `moisesinho`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `status`
+--
+ALTER TABLE `status`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `fk_status` FOREIGN KEY (`status`) REFERENCES `status` (`Id`),
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_moisesinho` FOREIGN KEY (`tipo`) REFERENCES `moisesinho` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
